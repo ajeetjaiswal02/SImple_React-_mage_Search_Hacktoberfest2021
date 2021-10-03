6ed1210ce22a5a133ae56456a4f568cdc8a5a4e3
shubham rana <rana2001shubham@gmail.com>
2021-10-03 12:21:13 +0530

    changed titles, improved UI, showing results for keyword statement added

--- src/component/Cards.js
+++ src/component/Cards.js
@@ -3,10 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card, Button } from 'react-bootstrap';
 function  Cards({photos}) {
   return (
-    <Card style={{ width: '25rem' }}>
-      <Card.Img variant="top" src={photos.urls.small}/>
+    <Card style={{ width: '25rem' }} className="border border-info rounded-top rounded-end mt-3">
+      <Card.Img variant="top" className="w-100" src={photos.urls.small}/>
       <Card.Body>
-        <Button variant="primary">{photos.alt_description}</Button>
+        <Button variant="primary" className="w-100 pt-2 pb-2">{photos.alt_description}</Button>
       </Card.Body>
     </Card>
   );
