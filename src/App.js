fa2e1f0309f89a4c3ccaf6655498ec028fee9e3f
JaiswalMudit <jaiswalmudit5@gmail.com>
2022-10-25 22:17:23 +0530

    Added a callback funtion

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import ParticleBackground from './component/ParticleBackground';
 import DarkModeToggle from "react-dark-mode-toggle";
 import Spinner from "./component/Spinner";
 
-
 function App() {
   const [phto, setphto] = useState("");
   const [result, setResult] = useState([]);
@@ -15,12 +14,10 @@ function App() {
   const [isDarkMode, setIsDarkMode] = useState(() => false);
   const [isLoading, setIsLoading] = useState(false);
   const clientid = "Your_API_KEY";
-  function handleChange(event) 
-  {
+  function handleChange(event) {
     setphto(event.target.value);
   }
-  function handleSumbit(event) 
-  {
+  function handleSumbit(event) {
     setIsLoading(true);
     event.preventDefault();
     console.log(phto);
@@ -58,36 +55,6 @@ function App() {
             name="photo"
             placeholder="Search"
           />
-          
-
-          form onSubmit={handleSumbit} className="input-group mb-3 w-50 m-auto pt-3">
-          <input
-            className="form-control"
-            onChange={handleChange}
-            type="search"
-            name="photo"
-            placeholder="Search"
-          />
-          
-          form onSubmit={handleSumbit} className="input-group mb-3 w-50 m-auto pt-3">
-          <input
-            className="form-control"
-            onChange={handleChange}
-            type="search"
-            name="photo"
-            placeholder="Search"
-          />
-          
-          form onSubmit={handleSumbit} className="input-group mb-3 w-50 m-auto pt-3">
-          <input
-            className="form-control"
-            onChange={handleChange}
-            type="search"
-            name="photo"
-            placeholder="Search"
-          />
-          
-
            <button
             type="submit"
             className="btn-primary btn"
@@ -100,24 +67,17 @@ function App() {
           ? <h3 className="text-secondary fs-5"> Enter Keywords To Search  </h3>
           : <h3 className="text-secondary fs-5"> Showing Results F  or "<span className="text-primary"> {searchterm} </span> " </h3>
         }
-        
-        {isLoading ?
-         (
+        {isLoading ? (
           <div className="w-100 mt-5 d-flex justify-content-center align-items-center">
             <Spinner />
           </div>
-        )
-         : 
-        (
+        ) : (
           <div className="w-100 d-flex justify-content-around flex-wrap">
             {result.map((phto) => (
               <Cards photos={phto} />
-            )
-            )
-            }
+            ))}
           </div>
-        )
-        }
+        )}
       </div>
       <DarkModeToggle
         className="dark-mode-toggle-btn"
@@ -128,5 +88,14 @@ function App() {
     </div>
   );
 }
-
+socket.on('new user', function(data, callback){
+  if (nicknames.indexOf(data) != -1){
+      callback(false);
+  } else{
+      callback(true);
+      socket.nickname = data;
+      nicknames.push(socket.nickname);
+      updateUserList();
+  }
+});
 export default App;
